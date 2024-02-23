4530fb5497b6bdcc579ac106b0c20cb3194fe3a4
= <=>
2024-02-23 19:12:30 -0300

    exploracao inicial

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,16 +2,34 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+#from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
+    # esse script deve ser executado por linha de comando da seguinte forma: python seu_script.py caminho_para_o_arquivo_de_entrada.csv caminho_para_o_arquivo_de_saida.csv
+
+    # leitura da base
+    df_casas_raw = pd.read_csv(r'{}'.format(input_filepath))
+
+    ######################## TRATAMENTO DOS NULOS   
+    # preenchimento dos nulos
+    num_cols = df_casas_raw.select_dtypes('float', 'int').columns
+    obj_cols = df_casas_raw.drop(columns='Electrical').select_dtypes('object').columns
+    df_casas_raw[num_cols] = df_casas_raw[num_cols].fillna(0)
+    df_casas_raw[obj_cols] = df_casas_raw[obj_cols].fillna('NotAvailable')
+
+    # excluindo linhas nulas
+    df_casas_raw = df_casas_raw.loc[ ~(df_casas_raw['Electrical'].isnull() )]
+    ########################
+    
+    # salvando os resultados
+    df_casas_raw.to_csv(r'{}'.format(output_filepath), index=False)
+    
+
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
@@ -25,6 +43,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    #load_dotenv(find_dotenv())
 
     main()
