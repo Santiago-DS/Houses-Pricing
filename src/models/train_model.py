f3129b933ab397a50ebdcb0602f37fa586f70aa2
Gabriel Santiago <gabriel.santiago050299@gmail.com>
2024-04-25 15:35:48 -0300

    Ajustes finais no treinamento dos modelos

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,7 +6,7 @@
 import pandas as pd
 import numpy as np
 import mlflow
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_validate, KFold, train_test_split
 from sklearn.metrics import make_scorer, r2_score
@@ -21,35 +21,39 @@ warnings.filterwarnings("ignore")
 ################################
 ##### CARREGAMENTO DOS DADOS
 ################################
-print('Script de treinamento do modelo!')
-print('Carregando os dados...')
-df = pd.read_csv(r"../../data/processed/train_casas.csv")
+
+def data_preprocessing():
+    #print('Script de treinamento do modelo!')
+    print('Carregando os dados...')
+    df = pd.read_csv(r"../../data/processed/train_casas.csv")
 
 
 
-################################
-##### DIVISAO DAS BASES
-################################
-print('Preparando as bases...')
-SEED = 42
-np.random.seed(SEED)
+    ################################
+    ##### DIVISAO DAS BASES
+    ################################
+    print('Preparando as bases...')
+    SEED = 42
+    np.random.seed(SEED)
 
-# dividindo em dados de treino e teste
-df_train, df_test = train_test_split(
-    df,
-    test_size=0.2,
-    shuffle=True,
-    random_state=SEED)
-X_train = df_train.drop(columns='SalePrice').astype('float').copy()
-y_train = df_train['SalePrice'].astype('float').values
-X_test= df_test.drop(columns='SalePrice').astype('float').copy()
-y_test= df_test['SalePrice'].astype('float').values
+    # dividindo em dados de treino e teste
+    df_train, df_test = train_test_split(
+        df,
+        test_size=0.2,
+        shuffle=True,
+        random_state=SEED)
+    X_train = df_train.drop(columns='SalePrice').astype('float').copy()
+    y_train = df_train['SalePrice'].astype('float').values
+    X_test= df_test.drop(columns='SalePrice').astype('float').copy()
+    y_test= df_test['SalePrice'].astype('float').values
+    return df, df_train, df_test, X_train, y_train, X_test, y_test
+
+df, df_train, df_test, X_train, y_train, X_test, y_test = data_preprocessing()
 
 ################################
 ##### PIPELINE DE TREINAMENTO
 ################################
-print('Treinando os modelos...')
-scaler = MinMaxScaler()
+scaler = StandardScaler()
 cv = KFold(n_splits=10, shuffle=True)
 #params = {}
 models = {
@@ -58,54 +62,63 @@ models = {
     'LinearRegression': LinearRegression(),
     'XGBRegressor': XGBRegressor()}
 models_r2score = []
-scores = make_scorer(r2_score)
+#scores = make_scorer(r2_score)
 
 # ! Verifique se a porta 5000 está liberada em seu computador antes de testar
-mlflow.set_tracking_uri('http://127.0.0.1:5000')
-mlflow.set_experiment('houses-pricing-AmericanHouses')
+def main():
+    print('Treinando os modelos...')
+    mlflow.set_tracking_uri('http://127.0.0.1:5000')
+    mlflow.set_experiment('houses-pricing-AmericanHouses')
 
-for name, model in zip(list(models.keys()), list(models.values())):
-    with mlflow.start_run():
-        if model == XGBRegressor():
-            mlflow.xgboost.log_model(model, name)
-        else:
-            mlflow.sklearn.log_model(model, name)
+    for name, model in zip(list(models.keys()), list(models.values())):
+        with mlflow.start_run():
+            if model == XGBRegressor():
+                mlflow.xgboost.log_model(model, name)
+            else:
+                mlflow.sklearn.log_model(model, name)
 
-        pipe = Pipeline( [('escalonamento', scaler), ('treinamento', model)] )
-        resultados = cross_validate(
-            pipe,
-            X_train,
-            y_train,
-            cv = cv,
-            return_train_score=True
-        )
-        model_test = pipe.fit(X_train, y_train)
-        predict_test = pipe.predict(X_test)
-        r2_score_test_database = r2_score(y_test, predict_test)
-        crossval_r2_score_test_medio = resultados['test_score'].mean()
-        crossval_r2_score_test_std = resultados['test_score'].std()
-        limite_superior = crossval_r2_score_test_medio + 2*crossval_r2_score_test_std
-        limite_inferior = crossval_r2_score_test_medio - 2*crossval_r2_score_test_std
+            pipe = Pipeline( [('escalonamento', scaler), ('treinamento', model)] )
+            resultados = cross_validate(
+                pipe,
+                X_train,
+                y_train,
+                cv = cv,
+                return_train_score=True
+            )
+
+            # criacao da pipeline e treinamento dos modelos
+            pipe.fit(X_train, y_train)
+            predict_test = pipe.predict(X_test)
+            r2_score_test_database = r2_score(y_test, predict_test)
+            crossval_r2_score_test_medio = resultados['test_score'].mean()
+            crossval_r2_score_test_std = resultados['test_score'].std()
+            limite_superior = crossval_r2_score_test_medio + 2*crossval_r2_score_test_std
+            limite_inferior = crossval_r2_score_test_medio - 2*crossval_r2_score_test_std
 
 
-        resultados[name] = r2_score_test_database 
+            resultados[name] = r2_score_test_database 
+
+            # Logando as metricas
+            mlflow.log_param("Estimator Name", name)
+            mlflow.log_param("Scaler Name", 'StandardScaler')
+            mlflow.log_metric('mean_crossval_test_r2_score', crossval_r2_score_test_medio*100)
+            mlflow.log_metric('std_database_r2_score_', crossval_r2_score_test_std*100)
+            mlflow.log_metric('limite_superior', limite_superior*100)
+            mlflow.log_metric('limite_inferior', limite_inferior*100)
+            mlflow.log_metric('test_database_r2_score_', r2_score_test_database*100)
+            models_r2score.append(r2_score_test_database)
+            print(f'Treinamento do modelo {name} concluido!')
+
+    models_result = {
+        'Modelo': list(models.keys()),
+        'r2_score': models_r2score
+    }
+
+    print('\nO melhor modelo foi selecionado com base no r2_score.\nO modelo selecionado foi:')
+    models_df = pd.DataFrame(models_result)
+    best_estimator_name = models_df.nlargest(columns='r2_score',n=1).Modelo.values[0]
+    print(best_estimator_name)
 
 
-        mlflow.log_param("Estimator Name", name)
-        mlflow.log_metric('mean_crossval_test_r2_score', crossval_r2_score_test_medio*100)
-        mlflow.log_metric('std_database_r2_score_', crossval_r2_score_test_std*100)
-        mlflow.log_metric('limite_superior', limite_superior*100)
-        mlflow.log_metric('limite_inferior', limite_inferior*100)
-        mlflow.log_metric('test_database_r2_score_', r2_score_test_database*100)
-        models_r2score.append(r2_score_test_database)
-        print(f'Treinamento do modelo {name} concluido!')
-
-models_result = {
-    'Modelo': list(models.keys()),
-    'r2_score': models_r2score
-}
-
-print('\nO melhor modelo foi selecionado com base no r2_score.\nO modelo selecionado foi:')
-models_df = pd.DataFrame(models_result)
-best_estimator_name = models_df.nlargest(columns='r2_score',n=1).Modelo.values[0]
-print(best_estimator_name)
+if __name__ == '__main__':
+    main()
